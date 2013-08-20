ab5979f78f4fce90189a442a695e1614c7f6939f
Wang Xiaoyang <wangxiaoyangll@hotmail.com>
2013-08-20 17:35:53 +0800

    Initial Commit

---
+++ BHG/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BHG
+//
+//  Created by Wang Xiaoyang on 8/20/13.
+//  Copyright (c) 2013 Wang Xiaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
